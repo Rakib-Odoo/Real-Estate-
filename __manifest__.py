679f45ff7586fc20776721bd2f754a1b3a7e6a87
Rakib-Odoo <shahidulhuda0@gmail.com>
2025-01-08 22:08:35 +0600

    create real estate module

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Real Estate',
+    'version': '15.0.1.1.1',
+    'description': 'Real estate module to show available properties',
+    'author': 'Rakib Hasan',
+    'category': 'Sales',
+    'sequence': 1,
+    'depends': [],
+    'data': [
+        # security files
+        'security/ir.model.access.csv',
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+
+}
---
+++ models/property.py
@@ -0,0 +1,33 @@
+from odoo import fields, models, api
+
+
+class Property(models.Model):
+    _name = 'real_estate.property'
+    _description = 'Property Details'
+
+    name = fields.Char(string='Property Name', required=True)
+    state = fields.Selection([
+        ('new','New'),
+        ('received','Offer Received'),
+        ('accepted','Offer Accepted'),
+        ('sold','Sold'),
+        ('canceled','Canceled'),
+    ], string='Status', default='new')
+    description = fields.Text(string='Description')
+    postcode = fields.Char(string='Postcode')
+    date_available = fields.Date(string='Available From')
+    expected_price = fields.Monetary(string='Expected Price')
+    best_offer = fields.Monetary(string='Best Offer', compute='_compute_best_offer')
+    selling_price = fields.Monetary(string='Selling Price', readonly=True)
+    bedroom = fields.Integer(string='Bedrooms')
+    living_area = fields.Float(string='Living Area (sqm)')
+    bathroom = fields.Integer(string='Bathrooms')
+    garage = fields.Boolean(string='Garages', default=False)
+    garden = fields.Boolean(string='Garden', default=False)
+    garden_area = fields.Float(string='Garden Area (sqm)')
+    gardem_orientation = fields.Selection([
+        ('north','North'),
+        ('south','South'),
+        ('east','East'),
+        ('west','West'),
+    ], string='Garden Orientation')
